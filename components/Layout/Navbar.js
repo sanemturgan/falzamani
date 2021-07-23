1d3d212e74eca7a0dd2627bb29052023566fa388
Furkan Çığırtkan <73472352+furkancgrtkn@users.noreply.github.com>
2021-07-24 00:26:57 +0300

    feat: dropdown menu

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,28 +1,33 @@
+import { useState, useRef } from "react";
 import classes from "../../styles/Navbar.module.css";
 import { FaAngleDown } from "react-icons/fa";
 import Image from "next/image";
 import Logo from "../../public/img/logo.png";
 import Link from "next/link";
-
+import useOutSideClick from "../../hooks/useOutSideClick";
 function Navbar() {
+  // Dropdown Menu
+  const menuRef = useRef();
+  const [menuIsOpen, openMenu] = useState(false);
+  useOutSideClick(menuRef, () => openMenu(false));
   return (
     <nav className={classes.navbarmain}>
       <div className={classes.logo}>
         <Link href="/homepage">
-          <div className={classes.imglg}>
-            <Image src={Logo} alt="logo" />
-          </div>
-          <h3>Fal Zamanı</h3>
+          <a>
+            <div className={classes.imglg}>
+              <Image src={Logo} alt="logo" />
+            </div>
+            <h3>Fal Zamanı</h3>
+          </a>
         </Link>
       </div>
       <ul className={classes.bar}>
-        <li>
-          <Link href="/faltürleri">
-            <a>
-              Fal Türleri
-              <FaAngleDown color="#140731" size="20px" />
-            </a>
-          </Link>
+        {/*  Dropdown Menu  */}
+        <li ref={menuRef} onClick={() => openMenu(!menuIsOpen)}>
+          Fal Türleri
+          <FaAngleDown color="#140731" size="20px" />
+          {menuIsOpen && <div className={classes.navbarDropdownMenu}>Menu</div>}
         </li>
         <li>
           <Link href="/uzmanlar">
@@ -30,19 +35,29 @@ function Navbar() {
           </Link>
         </li>
         <li>
-          <Link href="/burçlar">Burçlar</Link>
+          <Link href="/burclar">
+            <a>Burçlar</a>
+          </Link>
         </li>
         <li>
-          <Link href="/blog">Blog</Link>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
         </li>
         <li>
-          <Link href="/kredi">Kredi Al</Link>
+          <Link href="/kredi">
+            <a>Kredi Al</a>
+          </Link>
         </li>
         <li>
-          <Link href="/kariyer">Kariyer</Link>
+          <Link href="/kariyer">
+            <a>Kariyer</a>
+          </Link>
         </li>
         <li>
-          <Link href="/iletisim">Bize Ulaşın</Link>
+          <Link href="/iletisim">
+            <a>Bize Ulaşın</a>
+          </Link>
         </li>
       </ul>
       <div className={classes.giris}>
