26650d9bd782feaa6aca0d3fcdf6337d8f144d4c
miracosmanoglu <mrccc23@gmail.com>
2021-08-15 22:23:53 +0300

    feat: .

--- pages/kayit.js
+++ pages/kayit.js
@@ -18,26 +18,41 @@ import Link from "next/link";
 import axios from "axios";
 function Kayit() {
   const [name, setName] = useState("");
-  const unSubmit=()=>{
-    e.preventDefault()
-        let userObject = {
-           name: name,
-            surname: surname,
-            username:username,
-            email: mail,
-            phone:phone,
-            password: password,
-            
-        };
-        const regResponse = await axios.post(process.env.REACT_APP_CLIENT_API_URL + /user/signup, userObject)
-        console.log(regResponse)
+  const [phone, setPhone] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
 
-  }
+  const onSubmit = async () => {
+    let userObject = {
+      name: name,
+      surname: surname,
+      username: "username",
+      email: email,
+      phone: phone,
+      password: password,
+    };
+
+    // const regResponse = await axios.post(
+    //   "https://falzamani-backend.herokuapp.com/api/customer/register",
+    //   userObject
+    // );
+
+    const regResponse = await axios.get(
+      "https://falzamani-backend.herokuapp.com/api/warlock/all"
+    );
+
+    console.log(regResponse);
+  };
+
+  console.log(name, phone, email, password);
   return (
     <div className="kayit">
       <div className={classes.kariyerhdr}>
         <h4>KAYIT OL</h4>
       </div>
+
       <div className={classes.formkariyer}>
         <FormControl id="form">
           <FormLabel color="#fff" mb="16px" fontSize="18px">
@@ -55,16 +70,36 @@ function Kayit() {
           <FormLabel color="#fff" mb="16px" mt="16px" fontSize="18px">
             Telefon
           </FormLabel>
-          <Input placeholder="Telefon Numarası" size="lg" color="white" />
+          <Input
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            value={phone}
+            placeholder="Telefon Numarası"
+            size="lg"
+            color="white"
+          />
           <FormLabel color="#fff" mb="16px" mt="16px" fontSize="18px">
             Email
           </FormLabel>
-          <Input type="E-mail" size="lg" color="white" />
+          <Input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            type="E-mail"
+            size="lg"
+            color="white"
+          />
 
           <FormLabel color="#fff" mb="16px" mt="16px" fontSize="18px">
             Şifre
           </FormLabel>
           <Input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
             pr="4.5rem"
             mb="16px"
             type="password"
@@ -75,21 +110,22 @@ function Kayit() {
             Şifre Tekrar
           </FormLabel>
           <Input
+            onChange={(e) => {
+              setPassword2(e.target.value);
+            }}
+            value={password2}
             pr="4.5rem"
             mb="16px"
             type="password"
             size="lg"
             placeholder="Şifre Tekrar"
           />
+          <div className={classes.gonder}>
+            <button onClick={onSubmit} className={classes.gnd}>
+              Kayıt Ol
+            </button>
+          </div>
         </FormControl>
-
-        <div className={classes.gonder}>
-          <Link href="/Kayit">
-            <a>
-              <button className={classes.gnd}>Kayıt Ol</button>
-            </a>
-          </Link>
-        </div>
       </div>
     </div>
   );
