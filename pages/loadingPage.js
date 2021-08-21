94a9af9ae37b44a3f5e2f423bc49f0f8296b6fc2
sanemturgan <sanemturganilayda@gmail.com>
2021-08-22 00:20:48 +0300

    Feat:Edit.

---
+++ pages/loadingPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "../styles/LoadingPage.module.css";
+import { Button, ButtonGroup, BeatLoader } from "@chakra-ui/react";
+export default function search() {
+  return (
+    <div className="loading">
+      <div className={classes.loadingPage}>
+        <Button
+          isLoading
+          colorScheme="white"
+          type="lg"
+          /*spinner={<BeatLoader size={8} color="white" />}*/
+        >
+          Click Me
+        </Button>
+      </div>
+    </div>
+  );
+}
