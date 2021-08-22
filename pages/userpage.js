31b4480fc593939592cd4425f5536fd1bfc6c112
miracosmanoglu <mrccc23@gmail.com>
2021-08-22 19:06:28 +0300

    feat: chakra modal fixed

--- pages/userpage.js
+++ pages/userpage.js
@@ -51,7 +51,8 @@ export default function Userpage() {
       }
     }
     fetchData();
-  }, []);
+  }, [cookies, router]);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
   const {
     isOpen: isOpen2,
@@ -115,7 +116,7 @@ export default function Userpage() {
                     <Button
                       backgroundColor="inherit"
                       color="gray"
-                      onClick={onOpen}
+                      onClick={onOpen2}
                       mr="16px"
                     >
                       Yorum Yap
@@ -130,7 +131,7 @@ export default function Userpage() {
                           <GigEditable value="Yorum Yap" />
                         </ModalBody>
                         <ModalFooter>
-                          <Button colorScheme="red" mr={2} onClick={onClose}>
+                          <Button colorScheme="red" mr={2} onClick={onClose2}>
                             Çıkış
                           </Button>
                           <Button colorScheme="green" variant="ghost">
@@ -162,195 +163,6 @@ export default function Userpage() {
                           <Button colorScheme="blue" mr={3} onClick={onClose}>
                             Close
                           </Button>
-                        </ModalFooter>
-                      </ModalContent>
-                    </Modal>
-                  </div>
-                </div>
-              </li>
-              <li className={classes.btminfolist}>
-                <div className={classes.infofal}>
-                  <h6>Kahve Falı</h6>
-                </div>
-                <div className={classes.payanddetails}>
-                  <div className={classes.addComment}>
-                    <Button
-                      backgroundColor="inherit"
-                      color="gray"
-                      onClick={onOpen}
-                      mr="16px"
-                    >
-                      Yorum Yap
-                    </Button>
-
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalContent>
-                        <ModalHeader>Yorum Yap</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                          <GigEditable value="Yorum Yap" />
-                        </ModalBody>
-                        <ModalFooter>
-                          <Button colorScheme="red" mr={2} onClick={onClose}>
-                            Çıkış
-                          </Button>
-                          <Button colorScheme="green" variant="ghost">
-                            Kaydet
-                          </Button>
-                        </ModalFooter>
-                      </ModalContent>
-                    </Modal>
-                  </div>
-                  <div className={classes.details}>
-                    <Button
-                      backgroundColor="inherit"
-                      color="gray"
-                      onClick={onOpen}
-                    >
-                      Detay Oku
-                    </Button>
-
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalContent>
-                        <ModalHeader>Fal Detayı</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                          <p>Fal Detayı</p>
-                        </ModalBody>
-
-                        <ModalFooter>
-                          <Button colorScheme="blue" mr={3} onClick={onClose}>
-                            Close
-                          </Button>
-                          <Button variant="ghost">Secondary Action</Button>
-                        </ModalFooter>
-                      </ModalContent>
-                    </Modal>
-                  </div>
-                </div>
-              </li>
-              <li className={classes.btminfolist}>
-                <div className={classes.infofal}>
-                  <h6>Kahve Falı</h6>
-                </div>
-                <div className={classes.payanddetails}>
-                  <div className={classes.addComment}>
-                    <Button
-                      backgroundColor="inherit"
-                      color="gray"
-                      onClick={onOpen}
-                      mr="16px"
-                    >
-                      Yorum Yap
-                    </Button>
-
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalContent>
-                        <ModalHeader>Yorum Yap</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                          <GigEditable value="Yorum Yap" />
-                        </ModalBody>
-                        <ModalFooter>
-                          <Button colorScheme="red" mr={2} onClick={onClose}>
-                            Çıkış
-                          </Button>
-                          <Button colorScheme="green" variant="ghost">
-                            Kaydet
-                          </Button>
-                        </ModalFooter>
-                      </ModalContent>
-                    </Modal>
-                  </div>
-                  <div className={classes.details}>
-                    <Button
-                      backgroundColor="inherit"
-                      color="gray"
-                      onClick={onOpen}
-                    >
-                      Detay Oku
-                    </Button>
-
-                    <Modal isOpen={isOpen2} onClose={onClose2}>
-                      <ModalOverlay />
-                      <ModalContent>
-                        <ModalHeader>Fal Detayı</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                          <p>Fal Detayı</p>
-                        </ModalBody>
-
-                        <ModalFooter>
-                          <Button colorScheme="blue" mr={3} onClick={onClose}>
-                            Close
-                          </Button>
-                          <Button variant="ghost">Secondary Action</Button>
-                        </ModalFooter>
-                      </ModalContent>
-                    </Modal>
-                  </div>
-                </div>
-              </li>
-              <li className={classes.btminfolist}>
-                <div className={classes.infofal}>
-                  <h6>Kahve Falı</h6>
-                </div>
-                <div className={classes.payanddetails}>
-                  <div className={classes.addComment}>
-                    <Button
-                      backgroundColor="inherit"
-                      color="gray"
-                      onClick={onOpen}
-                      mr="16px"
-                    >
-                      Yorum Yap
-                    </Button>
-
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalContent>
-                        <ModalHeader>Yorum Yap</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                          <GigEditable value="Yorum Yap" />
-                        </ModalBody>
-                        <ModalFooter>
-                          <Button colorScheme="red" mr={2} onClick={onClose}>
-                            Çıkış
-                          </Button>
-                          <Button colorScheme="green" variant="ghost">
-                            Kaydet
-                          </Button>
-                        </ModalFooter>
-                      </ModalContent>
-                    </Modal>
-                  </div>
-                  <div className={classes.details}>
-                    <Button
-                      backgroundColor="inherit"
-                      color="gray"
-                      onClick={onOpen}
-                    >
-                      Detay Oku
-                    </Button>
-
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalContent>
-                        <ModalHeader>Fal Detayı</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                          <p>Fal Detayı</p>
-                        </ModalBody>
-
-                        <ModalFooter>
-                          <Button colorScheme="blue" mr={3} onClick={onClose}>
-                            Close
-                          </Button>
-                          <Button variant="ghost">Secondary Action</Button>
                         </ModalFooter>
                       </ModalContent>
                     </Modal>
