a792c629edb9f5ca35980cc6d7f48fd265ec7228
sanemturgan <sanemturganilayda@gmail.com>
2021-08-28 16:02:17 +0300

    Feat:Css edit.

--- components/NewGigModal.js
+++ components/NewGigModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import classes from "../styles/warlockpage.module.css";
+import classes from "../styles/NewGigModal.module.css";
 import GigEditable from "../components/GigEditable";
 
 import {
