6253bed744384e4ca7f8b99a79d214320bb345ce
miracosmanoglu <mrccc23@gmail.com>
2021-09-04 15:29:26 +0300

    feat: -next.config updated with nextappapiurl

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ module.exports = {
   },
   env: {
     REACT_APP_CLIENT_API_URL: "https://falzamani-backend.herokuapp.com/api",
+    NEXT_APP_API_URL: "https://falzamani-backend.herokuapp.com/api",
   },
 };
